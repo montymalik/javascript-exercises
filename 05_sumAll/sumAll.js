2b33dfa34cc2d5f0077b15e5f46d60b5f7ee3993
montymalik <monty.malik@gmail.com>
2022-03-22 16:31:45 -0400

    Completed!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let startTotal = 0;
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
 
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+
+    if (start > end) {
+        for (i = end; i <= start; i++) {
+            startTotal = i + startTotal;
+        }
+    } else {
+        for (i = start; i <= end; i++) {
+            startTotal = i + startTotal;
+    }
+}
+        return startTotal;
 };
 
 // Do not edit below this line
