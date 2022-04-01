875e8c3a560dd1cc0fbf51bf5162ae07dc950f3b
montymalik <monty.malik@gmail.com>
2022-04-01 14:23:44 -0400

    RegEx - important

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const replaced = str.toLowerCase().replace(/[^a-z]/g, "");
+    const reversed = replaced.split("").reverse().join("")
+    console.log(reversed)
+    if (reversed === replaced) {
+        return true
+    } else {
+        return false
+    }
 
 };
 
